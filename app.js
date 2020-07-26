633949bade1cb2fd4a64a999b160fd262144b9c4
SaintSmok <ce11d025@csms.edu.mn>
2020-07-26 15:28:34 +0800

    dice-g zagvarchilsan

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ var scores = [0, 0];
 // Идэвхитэй тоглогчийн ээлжийн цуглуулж буй оноо
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгатай байна.
-var dice = Math.floor(Math.random() * 6) + 1;
-/* <div class="player-score" id="score-0">43</div> */
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+// var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+// Програм эхлэх
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+// queryselector-oor olson elementee huwisagchidzarlaj bna.
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+// Shoog shideh
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
+// console.log("Шоо :" + diceNumber);
 
 // roll-dice tovchiig DOM-s avah
-
-console.log("Шоо :" + dice);
