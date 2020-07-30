1338c6ee9f2395d0a515c6222cebf9beccebf455
SaintSmok <ce11d025@csms.edu.mn>
2020-07-30 23:40:32 +0800

    button newgame-g hiih dutuu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглоом дууссан эсэхийг хадгалах төлөвийн хувьсагч
-var isGameOver;
+var isNewGame;
 
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
 var activePlayer = 0;
@@ -18,21 +18,19 @@ initGame();
 // Тоглоом эхлэхэд бэлтгэнэ.
 
 function initGame() {
-  isGameOver = false;
+  isNewGame = true;
+  // Програм эхлэхэд бэлтгэе
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  // document.querySelector(".player-name").textContent = "Winner";
+  globalThis.diceDom = document.querySelector(".dice");
+  diceDom.style.display = "none";
+  // Шоог шидэх эвент листенер
 }
-// Програм эхлэхэд бэлтгэе
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-// document.querySelector(".player-name").textContent = "Winner";
-
-var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
-// Шоог шидэх эвент листенер
-
-if (isGameOver !== true) {
-  document.querySelector(".btn-roll").addEventListener("click", function () {
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  if (isNewGame === true) {
     //   1-6 хүртэлх санамсаргүй 1 тоог гаргана.
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     // Шооны зургийг еб дээр гаргаж ирнэ.
@@ -41,7 +39,6 @@ if (isGameOver !== true) {
     diceDom.src = "dice-" + diceNumber + ".png";
 
     //   Тоглогчийн ээлжийг солих
-
     if (diceNumber !== 1) {
       roundScore += diceNumber;
       document.getElementById(
@@ -49,44 +46,43 @@ if (isGameOver !== true) {
       ).textContent = roundScore;
     } else {
       //     // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
-
       //     // 1 буусан тул тоглогчийн ээлжийг солино.
-
       changePlayer();
 
       // Мааааашшшш чухаллллллл !!!!!!!!
       // document.querySelector(".player-0-panel").classList.remove("active");
       // document.querySelector(".player-1-panel").classList.add("active");
-
       // Шоог түр алга болгох
     }
-  });
-}
+  }
+});
 
 //  Тоог хадгалах хэсэг Hold button event listener
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // Уг тоглогчийн цуглуулсан оноог глобаль оноон дээр нь нэмж өгнө.
-  // Ээлжийн оноог 0 болгоно.
-  // Тоглогчийн ээлжийг солино.
+  if (isNewGame === true) {
+    // Уг тоглогчийн цуглуулсан оноог глобаль оноон дээр нь нэмж өгнө.
+    // Ээлжийн оноог 0 болгоно.
+    // Тоглогчийн ээлжийг солино.
 
-  isGameOver = true;
-  scores[activePlayer] += roundScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-  // Тоглогч хожсон эсэхийг шалгах
-  if (scores[activePlayer] > 30) {
-    document.getElementById("name-" + activePlayer).textContent = "winner!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    // document.getElementById("current-" + activePlayer).textContent = roundScore;
+    scores[activePlayer] += roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+    // Тоглогч хожсон эсэхийг шалгах
+    if (scores[activePlayer] > 30) {
+      isNewGame = false;
+      document.getElementById("name-" + activePlayer).textContent = "winner!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      // document.getElementById("current-" + activePlayer).textContent = roundScore;
 
-    changePlayer();
+      changePlayer();
+    }
   }
 });
 
